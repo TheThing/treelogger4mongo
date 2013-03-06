a3407db9bbf3eac5ad8667799d5a9ff30572ff10
Jónatan Nilsson <jonatan@nilsson.is>
2013-03-06 02:02:44 +0000

    Finished implementing basic functionality of branch.

--- treelogger4mongo/branch.py
+++ treelogger4mongo/branch.py
@@ -1,4 +1,53 @@
+import datetime
+import getpass
+import threading
 
+class LogLevel:
+    DEBUG = 0
+    INFO = 1
+    WARNING = 2
+    ERROR = 3
+    CRITICAL = 4
 
 class Branch:
-	def __init__(self, tree)
+    def __init__(self, tree, caller, lvl, message, parent=None, data=None):
+        self._id = None
+        self.tree = tree
+        self.caller = caller
+        self.lvl = lvl
+        self.message = message
+        self.data = data
+        self.parent = parent._id if parent != None else parent
+        pass
+
+    def save(self):
+        self._id = self.tree.collection.insert(self.to_dict())
+        return self
+
+    def debug(self, message, data=None):
+        return self.tree.log(LogLevel.DEBUG, message, self, data)
+
+    def info(self, message, data=None):
+        return self.tree.log(LogLevel.INFO, message, self, data)
+
+    def warning(self, message, data=None):
+        return self.tree.log(LogLevel.WARNING, message, self, data)
+
+    def error(self, message, data=None):
+        return self.tree.log(LogLevel.ERROR, message, self, data)
+
+    def critical(self, message, data=None):
+        return self.tree.log(LogLevel.CRITICAL, message, self, data)
+
+    def to_dict(self):
+        return {
+            'time': datetime.datetime.now(),
+            'thread_ident': threading.current_thread().ident,
+            'user': getpass.getuser(),
+            'level': self.lvl,
+            'parent': self.parent,
+            'call_file': self.caller[1],
+            'call_function': self.caller[3],
+            'message': self.message,
+            'data': self.data,
+        }
