fa72ee00c1196ca12f062e3c0f353fd8059bbfe3
Jónatan Nilsson <jonatan@nilsson.is>
2013-03-06 03:01:49 +0000

    Added loglevel exception. Added get_friendly_name for levels. Added text-friendly version of level to dict.

--- treelogger4mongo/branch.py
+++ treelogger4mongo/branch.py
@@ -8,6 +8,13 @@ class LogLevel:
     WARNING = 2
     ERROR = 3
     CRITICAL = 4
+    EXCEPTION = 5
+
+    def get_friendly_name(lvl):
+        for p in dir(LogLevel):
+            if not p.startswith("_"):
+                if getattr(LogLevel, p) == lvl:
+                    return p
 
 class Branch:
     def __init__(self, tree, caller, lvl, message, parent=None, data=None):
@@ -39,12 +46,16 @@ class Branch:
     def critical(self, message, data=None):
         return self.tree.log(LogLevel.CRITICAL, message, self, data)
 
+    def exception(self, message=None, exception=None):
+        return self.tree.exception(message, exception, self)
+
     def to_dict(self):
         return {
             'time': datetime.datetime.now(),
             'thread_ident': threading.current_thread().ident,
             'user': getpass.getuser(),
             'level': self.lvl,
+            'level-text': LogLevel.get_friendly_name(self.lvl)
             'parent': self.parent,
             'call_file': self.caller[1],
             'call_function': self.caller[3],
